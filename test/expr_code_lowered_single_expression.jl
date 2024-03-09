df037ac9a2e5175d5f37f84011982107eab2db0c
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-03-09 13:15:21 -0500

    Add broken test

--- test/expr_code_lowered_single_expression.jl
+++ test/expr_code_lowered_single_expression.jl
@@ -9,3 +9,43 @@ import MultiBroadcastFusion as MBF
     expr_out = :(Base.materialize!(y1, Base.broadcasted(+, x1, x2, x3, x4)))
     @test MBF.code_lowered_single_expression(expr_in) == expr_out
 end
+
+@testset "code_lowered_single_expression - examples from the wild" begin
+    expr_in = quote
+        @. ᶜcloud_fraction = quad_loop(
+            SG_quad,
+            ᶜts,
+            Geometry.WVector(p.precomputed.ᶜgradᵥ_q_tot),
+            Geometry.WVector(p.precomputed.ᶜgradᵥ_θ_liq_ice),
+            coeff,
+            ᶜmixing_length,
+            thermo_params,
+        )
+    end
+    expr_out = :(Base.materialize!(
+        ᶜcloud_fraction,
+        Base.broadcasted(
+            quad_loop,
+            SG_quad,
+            ᶜts,
+            Base.broadcasted(
+                Base.getproperty(Geometry, :WVector),
+                Base.getproperty(
+                    Base.getproperty(p, :precomputed),
+                    :ᶜgradᵥ_q_tot,
+                ),
+            ),
+            Base.broadcasted(
+                Base.getproperty(Geometry, :WVector),
+                Base.getproperty(
+                    Base.getproperty(p, :precomputed),
+                    :ᶜgradᵥ_θ_liq_ice,
+                ),
+            ),
+            coeff,
+            ᶜmixing_length,
+            thermo_params,
+        ),
+    ))
+    @test_broken MBF.code_lowered_single_expression(expr_in) == expr_out
+end
