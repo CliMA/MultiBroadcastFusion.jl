958083a42cd4a11de777387190d6fd3f6e2846b2
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-04-09 15:46:15 -0400

    Move code_lowered_single_expression to sep file
    Remove fused_pairs macro
    
    wip

--- src/MultiBroadcastFusion.jl
+++ src/MultiBroadcastFusion.jl
@@ -13,7 +13,10 @@ Base.@propagate_inbounds rcopyto_at!(pairs::Tuple{<:Any}, i...) =
     rcopyto_at!(first(pairs), i...)
 @inline rcopyto_at!(pairs::Tuple{}, i...) = nothing
 
-include("macro_utils.jl")
+include("utils.jl")
+include("code_lowered_single_expression.jl")
+include("fused_pairs.jl")
+include("fused_pairs_flexible.jl")
 
 """
     @make_fused type_name fused_named
@@ -53,6 +56,52 @@ macro make_fused(type_name, fused_name)
         end
         macro $f(expr)
             _pairs = esc($(fused_pairs)(expr))
+            t = $t
+            quote
+                Base.copyto!($t($_pairs))
+            end
+        end
+    end
+end
+
+"""
+    @make_fused_flexible type_name fused_named
+
+This macro
+ - Imports MultiBroadcastFusion
+ - Defines a type, `type_name`
+ - Defines a macro, `@fused_name`
+
+This allows users to flexibility
+to customize their broadcast fusion.
+
+# Example
+```julia
+import MultiBroadcastFusion as MBF
+MBF.@make_fused_flexible MyFusedBroadcast my_fused
+
+Base.copyto!(fmb::MyFusedBroadcast) = println("You're ready to fuse!")
+
+x1 = rand(3,3)
+y1 = rand(3,3)
+y2 = rand(3,3)
+
+# 4 reads, 2 writes
+@my_fused begin
+  @. y1 = x1
+  @. y2 = x1
+end
+```
+"""
+macro make_fused_flexible(type_name, fused_name)
+    t = esc(type_name)
+    f = esc(fused_name)
+    return quote
+        struct $t{T <: Tuple}
+            pairs::T
+        end
+        macro $f(expr)
+            _pairs = esc($(fused_pairs_flexible)(expr, gensym()))
             t = $t
             quote
                 Base.copyto!($t($_pairs))
